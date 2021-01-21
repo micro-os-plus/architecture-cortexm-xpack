20617a90dbc3a2a6bcf101c57aee9667967fda2a
Liviu Ionescu <ilg@livius.net>
2021-01-21 21:22:33 +0200

    add initialise-interrupts-stack.cpp (moved here from rtos)

---
+++ src/startup/initialise-interrupts-stack.cpp
@@ -0,0 +1,57 @@
+/*
+ * This file is part of the µOS++ distribution.
+ *   (https://github.com/micro-os-plus)
+ * Copyright (c) 2021 Liviu Ionescu.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#if (!(defined(__APPLE__) || defined(__linux__) || defined(__unix__))) \
+    || defined(__DOXYGEN__)
+
+// ----------------------------------------------------------------------------
+
+#include <micro-os-plus/rtos/os.h>
+
+// ----------------------------------------------------------------------------
+
+using namespace os;
+
+// ----------------------------------------------------------------------------
+
+void
+os_startup_initialize_interrupts_stack (void* stack_begin_address,
+                                        size_t stack_size_bytes)
+{
+  trace::printf ("%s(%p,%u)\n", __func__, stack_begin_address,
+                 stack_size_bytes);
+
+  os::rtos::interrupts::stack ()->set (
+      (os::rtos::thread::stack::element_t*)stack_begin_address,
+      stack_size_bytes);
+}
+
+// ----------------------------------------------------------------------------
+
+#endif // ! Unix
+
+// ----------------------------------------------------------------------------
