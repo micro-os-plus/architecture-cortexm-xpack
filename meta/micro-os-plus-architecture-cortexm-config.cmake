ba47c6bd78b13584211377298e4196a0e0f54c5e
Liviu Ionescu <ilg@livius.net>
2021-02-25 16:42:03 +0200

    cmake: simplify, keep only config.cmake

--- meta/micro-os-plus-architecture-cortexm-config.cmake
+++ meta/micro-os-plus-architecture-cortexm-config.cmake
@@ -12,11 +12,55 @@
 # https://cmake.org/cmake/help/v3.18/
 # https://cmake.org/cmake/help/v3.18/manual/cmake-packages.7.html#package-configuration-file
 
-# message(STATUS "Including micro-os-plus-architecture-cortexm-config...")
+if(micro-os-plus-architecture-cortexm-included)
+  return()
+endif()
+
+set(micro-os-plus-architecture-cortexm-included TRUE)
+
+message(STATUS "Including micro-os-plus-architecture-cortexm...")
+
+# -----------------------------------------------------------------------------
+# Local dependencies.
+
+include("${CMAKE_CURRENT_LIST_DIR}/../device/meta/xpack-helper.cmake")
+
+# -----------------------------------------------------------------------------
+# The current folder.
+
+get_filename_component(xpack_current_folder ${CMAKE_CURRENT_LIST_DIR} DIRECTORY)
 
 # -----------------------------------------------------------------------------
 
-include("${CMAKE_CURRENT_LIST_DIR}/xpack-helper.cmake")
+if(NOT TARGET micro-os-plus-architecture-cortexm-interface)
+
+  add_library(micro-os-plus-architecture-cortexm-interface INTERFACE EXCLUDE_FROM_ALL)
+
+  # ---------------------------------------------------------------------------
+  # Target settings.
+
+  target_sources(
+    micro-os-plus-architecture-cortexm-interface
+
+    PRIVATE
+      # None so far, all are device dependent.
+  )
+
+  target_include_directories(
+    micro-os-plus-architecture-cortexm-interface
+
+    INTERFACE
+      ${xpack_current_folder}/include
+  )
+
+  # ---------------------------------------------------------------------------
+  # Aliases.
+
+  add_library(micro-os-plus::architecture-cortexm ALIAS micro-os-plus-architecture-cortexm-interface)
+  message(STATUS "micro-os-plus::architecture-cortexm")
+  add_library(micro-os-plus::architecture ALIAS micro-os-plus-architecture-cortexm-interface)
+  message(STATUS "micro-os-plus::architecture")
+
+endif()
 
 # -----------------------------------------------------------------------------
-
